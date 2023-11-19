6ee46d3a22a990d118f93b8b9158517a443a800c
Erik Hendrikson <erik.hendrikson@sustainability.games>
2023-11-19 19:56:05 +0100

    updated values

--- src/app/data/parties-list-data.ts
+++ src/app/data/parties-list-data.ts
@@ -1,25 +1,25 @@
 import { Party } from "../models/party.model";
 
 export const partiesListData: Party[] = [
-    { id: 1, label: 'VVD', value: 26, color: '#0B4EA2' }, // Blue
-    { id: 2, label: 'D66', value: 10, color: '#00A859' }, // Green
-    { id: 3, label: 'GL/PVDA', value: 12, color: '#4CAF50' }, // Green
-    { id: 4, label: 'PVV', value: 14, color: '#2C3E50' }, // Dark Blue
-    { id: 5, label: 'CDA', value: 8, color: '#00714D' }, // Dark Green
-    { id: 6, label: 'SP', value: 10, color: '#E30613' }, // Red
-    { id: 7, label: 'FVD', value: 10, color: '#000080' }, // Navy
-    { id: 8, label: 'Partij voor de Dieren', value: 5, color: '#3AA72D' }, // Green
-    { id: 9, label: 'ChristenUnie', value: 4, color: '#0071BC' }, // Blue
-    { id: 10, label: 'Volt', value: 3, color: '#057DBA' }, // Blue
-    { id: 11, label: 'JA21', value: 1, color: '#0A2C4E' }, // Navy
-    { id: 12, label: 'SGP', value: 1, color: '#4E2F3A' }, // Maroon
-    { id: 13, label: 'DENK', value: 1, color: '#FF5000' }, // Orange
-    { id: 14, label: 'BBB', value: 16, color: '#5F0080' }, // Purple
-    { id: 15, label: '50PLUS', value: 1, color: '#EB1D24' }, // Red
-    { id: 16, label: 'BIJ1', value: 1, color: '#2D308E' }, // Dark Blue
-    { id: 17, label: 'Nieuw Sociaal Contract', value: 4, color: '#0074B0' }, // Blue
-    { id: 18, label: 'BVNL/Groep Van Haga', value: 1, color: '#004080' }, // Dark Blue
-    { id: 19, label: 'Piratenpartij-De Groenen', value: 1, color: '#5BBD2C' }, // Green
-    { id: 20, label: 'Splinter', value: 1, color: '#0B7D62' }, // Teal
-    { id: 21, label: 'Libertaire Partij', value: 1, color: '#FFD700' } // Gold
+    { id: 1, label: 'VVD', value: 26, color: '#0B4EA2' },
+    { id: 2, label: 'D66', value: 8, color: '#00A859' },
+    { id: 3, label: 'GL/PVDA', value: 23, color: '#4CAF50' },
+    { id: 4, label: 'PVV', value: 26, color: '#2C3E50' },
+    { id: 5, label: 'CDA', value: 6, color: '#00714D' },
+    { id: 6, label: 'SP', value: 5, color: '#E30613' },
+    { id: 7, label: 'FVD', value: 5, color: '#000080' },
+    { id: 8, label: 'Partij voor de Dieren', value: 5, color: '#3AA72D' },
+    { id: 9, label: 'ChristenUnie', value: 3, color: '#0071BC' },
+    { id: 10, label: 'Volt', value: 4, color: '#057DBA' },
+    { id: 11, label: 'JA21', value: 1, color: '#0A2C4E' },
+    { id: 12, label: 'SGP', value: 2, color: '#4E2F3A' },
+    { id: 13, label: 'DENK', value: 4, color: '#FF5000' },
+    { id: 14, label: 'BBB', value: 7, color: '#5F0080' },
+    { id: 15, label: '50PLUS', value: 0, color: '#EB1D24' },
+    { id: 16, label: 'BIJ1', value: 1, color: '#2D308E' },
+    { id: 17, label: 'Nieuw Sociaal Contract', value: 23, color: '#0074B0' },
+    { id: 18, label: 'BVNL/Groep Van Haga', value: 1, color: '#004080' },
+    { id: 19, label: 'Piratenpartij-De Groenen', value: 0, color: '#5BBD2C' },
+    { id: 20, label: 'Splinter', value: 0, color: '#0B7D62' },
+    { id: 21, label: 'Libertaire Partij', value: 0, color: '#FFD700' }
 ];
